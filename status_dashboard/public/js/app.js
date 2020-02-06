27bea309243cfe9af68579374b408d150d83e7b2
Anuj Bhatia <anujbhatia83@gmail.com>
2020-02-07 09:43:30 +1000

    removing service worker

--- status_dashboard/public/js/app.js
+++ status_dashboard/public/js/app.js
@@ -1,7 +1,7 @@
-if (navigator.serviceWorker) {
+/*if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/update_status/status_dashboard/sw.js').then(function(registration) {
     console.log('ServiceWorker registration successful with scope:',  registration.scope);
   }).catch(function(error) {
     console.log('ServiceWorker registration failed:', error);
   });
-}
+}*/
